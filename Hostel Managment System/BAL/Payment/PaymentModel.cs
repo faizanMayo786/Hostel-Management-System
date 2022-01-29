a862098ddefbfd1de681781366a9f1b1e40f641e
Asad-Faiz <Muhammadasadfaiz2000@gmail.com>
2022-01-29 21:02:58 +0500

    Payment Model Added

---
+++ Hostel Managment System/BAL/Payment/PaymentModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hostel_Managment_System.BAL.Payment
+{
+   
+class PaymentModel
+    {
+        public string ID { get; set; }
+        public string Date { get; set; }
+        public PaymentModel(string id, string date)
+        {
+            ID = id;
+            Date = date;
+        }
+    }
+
+
+}
