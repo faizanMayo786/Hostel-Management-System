240fc20d1df4db178d92c269cf075a2b5d805b82
Asad-Faiz <Muhammadasadfaiz2000@gmail.com>
2022-01-29 21:00:16 +0500

    Allottee Model Added

--- Hostel Managment System/BAL/Allotte/AllotteeModel.cs
+++ Hostel Managment System/BAL/Allotte/AllotteeModel.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Hostel_Managment_System.BAL.Allotte
 {
-    class Allotte
+    class AllotteeModel
     {
-    }
+        
+            public string AllotteeID { get; set; }
+            public string CNIC { get; set; }
+            public string DOB { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+
+
+
+            public AllotteeModel(string allotteeID, string cnic, string dob, string phoneNumber, string name, string address)
+            {
+                AllotteeID = allotteeID;
+                CNIC = cnic;
+                Name = name;
+                DOB = dob;
+                PhoneNumber = phoneNumber;
+                Address = address;
+            }
+        }
+    
 }
